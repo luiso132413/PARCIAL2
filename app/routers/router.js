8598a8258d7c174686cd8b78d1c9ee4ca2160847
luiso132413 <loviedog@miumg.edu.gt>
2024-09-25 18:51:50 -0600

    3er commit

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,13 +1,29 @@
-
 let express = require('express');
 let router = express.Router();
  
-const song = require('../controllers/user.controller.js');
+const user = require('../controllers/user.controller.js');
+const projects = require('../controllers/projects.controller.js');
+const tasks = require('../controllers/tasks.controller.js');
 
-router.post('/api/song/create', song.create);
-router.get('/api/song/all', song.retrieveAllSong);
-router.get('/api/song/onebyid/:id', song.getSongById);
-router.put('/api/song/update/:id', song.updateById);
-router.delete('/api/song/delete/:id', song.deleteById);
+// Rutas de los Usuarios
+router.post('/api/user/create', user.create);
+router.get('/api/user/all', user.retrieveAllUsers);
+router.get('/api/user/onebyid/:id', user.getUserById);
+router.put('/api/user/update/:id', user.updateById);
+router.delete('/api/user/delete/:id', user.deleteById);
+
+// Rutas de los Proyectos
+router.post('/api/project/create', projects.create);
+router.get('/api/project/all', projects.retrieveAllProjects);
+router.get('/api/project/onebyid/:id', projects.getProjectById);
+router.put('/api/project/update/:id', projects.updateById);
+router.delete('/api/project/delete/:id', projects.deleteById);
+
+// Rutas de las Tareas
+router.post('/api/task/create', tasks.create);
+router.get('/api/task/all', tasks.retrieveAllTasks);
+router.get('/api/task/onebyid/:id', tasks.getTaskById);
+router.put('/api/task/update/:id', tasks.updateById);
+router.delete('/api/task/delete/:id', tasks.deleteById);
 
 module.exports = router;
